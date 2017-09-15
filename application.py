76fe61b6d34a21f3b8f0e77ca14d93923c6e4b1f
Diwaker Ghimire <idwaker@gmail.com>
2017-09-15 09:20:56 +0545

    home page with dummy data

--- application.py
+++ application.py
@@ -2,17 +2,15 @@
 
 import os
 # import pandas as pd
-# import sqlite3
 # from hashlib import sha256 #passlib
 # used to change the filename to secure format
 # from werkzeug.utils import secure_filename
 
 ## import application modules
-# import click
+import click
 from flask import Flask, render_template, request, g, redirect, flash
 # from flask_login import LoginManager, login_user, login_required, logout_user
-# from flask_sqlalchemy import SQLAlchemy
-# from deprecation import deprecated
+from flask_sqlalchemy import SQLAlchemy
 
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
@@ -21,43 +19,17 @@ print(BASEPATH)
 
 # create a new web application object
 app = Flask(__name__)
+
 # app.config['UPLOAD_FOLDER'] = os.path.join(BASEPATH, 'static/uploads')
 # app.config['SECRET_KEY'] = 'kaskdjh9213nkwej923fnkwvjnc92kejrvnkv93vkejv93'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEPATH, 'data/db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEPATH, 'data/db.sqlite3')
 
 # ALLOWED_EXTENSIONS = {'.csv',}
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # login_manager = LoginManager()
 # login_manager.init_app(app)
-
-
-# deprecated
-# DATABASE = 'data/db.sqlite3'
-
-
-# deprecated
-# def get_db():
-#     """
-#     connect to database if not already connected and
-#     return connection object
-#     """
-#     # g._database if g._database exists else None
-#     # getattribute
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         # g._database = sqlite3.connect(DATABASE)
-#         # db = g._database
-#         db = g._database = sqlite3.connect(DATABASE)
-#     return db
-
-
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
 
 """
 ORM (Object Relational Mapper)
@@ -92,13 +64,13 @@ as class and its instances.
 #         return self.id
 
 
-# class Dataset(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(120), unique=True)
-#     dataset = db.Column(db.String(250))
+class Dataset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    data = db.Column(db.String(250))
     
-#     def __repr__(self):
-#         return "{}, {}".format(self.name, self.dataset)
+    def __repr__(self):
+        return "{}, {}".format(self.name, self.data)
 
 
 # @login_manager.user_loader
@@ -120,23 +92,18 @@ as class and its instances.
 
 
 # add a new route 
-# http://localhost:5000/
 @app.route('/')
 def index():
     """
     this route will be run when a user visits main page of our application.
+    http://localhost:5000/
     """
     # using hardcoded records
-    records = [{'name': 'Hello', 'dataset': 'hello.csv'}]
+    # records = [{'name': 'Hello', 'dataset': 'hello.csv'}]
 
-    # get records from database using sql query
-    # cur = get_db().cursor()
-    # cur.execute('SELECT id, name, dataset from dataset')
-    # records = cur.fetchall()
-
-
-    # records = Dataset.query.all()
-    # print(records)
+    # get all rows from dataset table
+    records = Dataset.query.all()
+    print(records)
     return render_template('home.html', records=records)
 
 
@@ -269,48 +236,33 @@ def list_data():
 #     return redirect('/data/list')
 
 
-# @app.cli.command()
-# def initdb():
-#     """
-#     this is used to initialize database using command line
-#     """
-#     click.echo("Creating new tables...")
-#     # db = get_db()
-#     # db.execute("CREATE TABLE user(id INTEGER, username TEXT, password TEXT, status INTEGER)")
-#     # db.execute("CREATE TABLE dataset(id INTEGER, name TEXT, dataset TEXT)")
-#     # db.commit()
+@app.cli.command()
+def initdb():
+    """
+    this is used to initialize database using command line
+    """
+    click.echo("Creating new tables...")
 
-#     # using sqlalchemy
-#     db.create_all()
-#     click.echo("...done")
+    # using sqlalchemy
+    db.create_all()
+    click.echo("...done")
 
 
-# @app.cli.command()
-# def populate():
-#     """
-#     this is used to populate all the database tables using dummy data
-#     """
-#     click.echo("Loading data...")
-#     # db = get_db()
-#     # cur = db.cursor()
-#     # cur.execute("INSERT INTO user(id, username, password, status) VALUES(?, ?, ?, ?)",
-#     #             (1, 'admin', 'admin123', 1))
-#     # cur.executemany("INSERT INTO dataset(id, name, dataset) VALUES(?, ?, ?)",
-#     #                 [(1, 'type1', 'type1.csv'), (2, 'type2', 'type2.csv'),
-#     #                  (3, 'type3', 'type3.csv'), (4, 'type4', 'type4.csv'),
-#     #                  (5, 'type5', 'type5.csv'), (6, 'type6', 'type6.csv'),
-#     #                  (7, 'type7', 'type7.csv'), (8, 'type8', 'type8.csv'),
-#     #                  (9, 'type9', 'type9.csv'), (10, 'type10', 'type10.csv')])
-#     # db.commit()
-#     user = User(username='admin', password=sha256(b'admin123').hexdigest(), status=1)
-#     db.session.add(user)
-#     dataset = [(1, 'type1', 'type1.csv'), (2, 'type2', 'type2.csv'),
-#                      (3, 'type3', 'type3.csv'), (4, 'type4', 'type4.csv'),
-#                      (5, 'type5', 'type5.csv'), (6, 'type6', 'type6.csv'),
-#                      (7, 'type7', 'type7.csv'), (8, 'type8', 'type8.csv'),
-#                      (9, 'type9', 'type9.csv'), (10, 'type10', 'type10.csv')]
-#     for d in dataset:
-#         db.session.add(Dataset(name=d[1], dataset=d[2]))
-#     db.session.commit()
-#     click.echo("...done")
+@app.cli.command()
+def populate():
+    """
+    this is used to populate all the database tables using dummy data
+    """
+    click.echo("Loading data...")
+    # user = User(username='admin', password=sha256(b'admin123').hexdigest(), status=1)
+    # db.session.add(user)
+    dataset = [(1, 'type1', 'type1.csv'), (2, 'type2', 'type2.csv'),
+               (3, 'type3', 'type3.csv'), (4, 'type4', 'type4.csv'),
+               (5, 'type5', 'type5.csv'), (6, 'type6', 'type6.csv'),
+               (7, 'type7', 'type7.csv'), (8, 'type8', 'type8.csv'),
+               (9, 'type9', 'type9.csv'), (10, 'type10', 'type10.csv')]
+    for d in dataset:
+        db.session.add(Dataset(name=d[1], data=d[2]))
+    db.session.commit()
+    click.echo("...done")
 
